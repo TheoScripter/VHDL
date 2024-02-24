2b015f0443fd32c12bc0651c178454f1a35d8e7e
Théo <136507456+TheoScripter@users.noreply.github.com>
2024-02-24 12:50:43 +0100

    Program UART_Controller

---
+++ UART/UART_Controller.vhdl
@@ -0,0 +1,28 @@
+entity UART_Controller is
+    Port ( clk : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           rx_data_received : in STD_LOGIC;
+           tx_ready : in STD_LOGIC;
+           xon : in STD_LOGIC;
+           xoff : in STD_LOGIC;
+           interrupt : out STD_LOGIC);
+end UART_Controller;
+
+architecture Behavioral of UART_Controller is
+    signal interrupt_flag : BOOLEAN := FALSE;
+begin
+    process (clk, reset)
+    begin
+        if reset = '1' then
+            interrupt_flag <= FALSE;
+        elsif rising_edge(clk) then
+            if rx_data_received = '1' or (tx_ready = '1' and xon = '1') or (tx_ready = '0' and xoff = '1') then
+                interrupt_flag <= TRUE;
+            else
+                interrupt_flag <= FALSE;
+            end if;
+        end if;
+    end process;
+
+    interrupt <= '1' when interrupt_flag else '0';
+end Behavioral;
